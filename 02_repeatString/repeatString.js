8c7de2864a1529158b586ae50793117cb26f6083
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 19:36:36 -0500

    Complete 02_repeatString
    Fix repeatString.spec.js, all tests to pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = string;
+    if (num === 0) {
+        return '';
+    }
+    else if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 1; i < num; i++) {
+        result += string;
+    }
+    return result;
 };
+
+console.log(repeatString('hey', 6));
 
 // Do not edit below this line
 module.exports = repeatString;
