8917efa676dc34559a218fa7199fa867026d4cbf
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 21:03:15 -0500

    Complete 05_sumAll
    Write function to return sum between two ints.
    All tests pass.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    else if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    let result = 0;
+    if (end < start) {
+        for (let i = end; i <= start; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
