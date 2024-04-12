547554eb160e2d93e99e53daf959a0b40158981b
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 22:34:54 -0500

    Complete 07_tempConversion
    Write functions to convert to/from Celsius and Fahrenheit.
    All tests pass.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let newCelTemp = 0;
+  if (fahrenheitTemp === 32) {
+    return newCelTemp;
+  }
+  newCelTemp = parseFloat(((fahrenheitTemp - 32) * 5 / 9).toFixed(1));
+  return newCelTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let newFahTemp = 0;
+  if (celsiusTemp === 0) {
+    newFahTemp = 32;
+    return newFahTemp;
+  }
+  newFahTemp = parseFloat((celsiusTemp * (9 / 5) + 32).toFixed(1));
+  return newFahTemp;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
