2a7ab5dbb1b3acff5db1d67998b4b9ffea337513
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 19:44:56 -0500

    Complete 03_reverseString
    write function in reverseString.js, all tests pass.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = string.split('');
+    stringReversed = stringReversed.reverse();
+    stringReversed = stringReversed.join('');
+    return stringReversed;
 };
 
 // Do not edit below this line
