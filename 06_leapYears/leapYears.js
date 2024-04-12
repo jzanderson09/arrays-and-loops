af08cbedca9ee330dff93b95c00d0aba8cd295ce
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 21:20:21 -0500

    Complete 06_leapYears
    Write function to return status of leap year as boolean.
    All tests pass.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // divisible by 4
+    /*
+    years divisible by 100 are not leap 
+    years unless divisible by 400.
+    */
 
+    let result = false;
+    if (year % 4 === 0) {
+        result = true;
+        if (year % 100 === 0) {
+            result = false;
+            if (year % 400 === 0) {
+                result = true;
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
