414b7b104a0bb7fa2c1c74122d7f82939506f58c
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 20:40:12 -0500

    Complete 04_removeFromArray
    Write function to remove item(s) from array.
    All tests pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...item) {
+    let newArr = arr;
+    for (let i = 0; i < item.length; i++) {
+        newArr = newArr.filter(element => element !== item[i]);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
